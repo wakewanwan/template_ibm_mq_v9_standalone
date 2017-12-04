5722ae1d74162f1450a708b9146dea45b243dd10
CAMHub PublishBot <travis@example.org>
2017-12-04 22:44:21 +0000

    Automated CAMHub::publish on 2017-12-04 22:44:21 UTC

--- ibmcloud/terraform/ibm_mq_v9_standalone.tf
+++ ibmcloud/terraform/ibm_mq_v9_standalone.tf
@@ -11,31 +11,26 @@
 ##############################################################
 # Keys - CAMC (public/private) & optional User Key (public) 
 ##############################################################
-variable "user_public_ssh_key" {
-  type = "string"
-  description = "User defined public SSH key used to connect to the virtual machine. The format must be in openSSH."
-  default = "None"
-}
-
-variable "ibm_pm_public_ssh_key" {
-  description = "Public CAMC SSH key value which is used to connect to a guest, used on VMware only."
+variable "ibm_pm_public_ssh_key_name" {
+  description = "Public CAMC SSH key name used to connect to the virtual guest."
 }
 
 variable "ibm_pm_private_ssh_key" {
   description = "Private CAMC SSH key (base64 encoded) used to connect to the virtual guest."
 }
 
-variable "allow_unverified_ssl" {
-  description = "Communication with vsphere server with self signed certificate"
-  default = "true"
+variable "user_public_ssh_key" {
+  type = "string"
+  description = "User defined public SSH key used to connect to the virtual machine. The format must be in openSSH."
+  default = "None"
 }
 
 ##############################################################
-# Define the vsphere provider 
+# Define the ibm provider 
 ##############################################################
-provider "vsphere" {
-  allow_unverified_ssl = "${var.allow_unverified_ssl}"
-  version = "~> 0.4"
+#define the ibm provider
+provider "ibm" {
+  version = "~> 0.5"
 }
 
 provider "camc" {
@@ -44,6 +39,14 @@ provider "camc" {
 
 provider "random" {
   version = "~> 1.0"
+}
+
+##############################################################
+# Reference public key in Devices>Manage>SSH Keys in SL console) 
+##############################################################
+data "ibm_compute_ssh_key" "ibm_pm_public_key" {
+  label = "${var.ibm_pm_public_ssh_key_name}"
+  most_recent = "true"
 }
 
 resource "random_id" "stack_id" {
@@ -59,6 +62,14 @@ variable "ibm_stack_name" {
 }
 
 #### Default OS Admin User Map ####
+variable "default_os_admin_user" {
+  type        = "map"
+  description = "look up os_admin_user using resource image"
+  default = {
+    UBUNTU_16_64 = "root"
+    REDHAT_7_64 = "root"
+  }
+}
 
 ##### Environment variables #####
 #Variable : ibm_pm_access_token
@@ -98,6 +109,14 @@ variable "ibm_sw_repo_user" {
 variable "MQV9Node01-image" {
   type = "string"
   description = "Operating system image id / template that should be used when creating the virtual image"
+  default = "REDHAT_7_64"
+}
+
+#Variable : MQV9Node01-mgmt-network-public
+variable "MQV9Node01-mgmt-network-public" {
+  type = "string"
+  description = "Expose and use public IP of virtual machine for internal communication"
+  default = "true"
 }
 
 #Variable : MQV9Node01-name
@@ -295,127 +314,106 @@ variable "MQV9Node01_wmq_version" {
 }
 
 
+##### ungrouped variables #####
+##### domain name #####
+variable "runtime_domain" {
+  description = "domain name"
+  default = "cam.ibm.com"
+}
+
+
 #########################################################
 ##### Resource : MQV9Node01
 #########################################################
 
-variable "MQV9Node01-os_password" {
-  type = "string"
-  description = "Operating System Password for the Operating System User to access virtual machine"
-}
 
-variable "MQV9Node01_folder" {
-  description = "Target vSphere folder for virtual machine"
-}
-
+#Parameter : MQV9Node01_datacenter
 variable "MQV9Node01_datacenter" {
-  description = "Target vSphere datacenter for virtual machine creation"
-}
-
-variable "MQV9Node01_domain" {
-  description = "Domain Name of virtual machine"
-}
-
-variable "MQV9Node01_number_of_vcpu" {
-  description = "Number of virtual CPU for the virtual machine, which is required to be a positive Integer"
-  default = "2"
-}
-
-variable "MQV9Node01_memory" {
-  description = "Memory assigned to the virtual machine in megabytes. This value is required to be an increment of 1024"
-  default = "2048"
-}
-
-variable "MQV9Node01_cluster" {
-  description = "Target vSphere cluster to host the virtual machine"
-}
-
-variable "MQV9Node01_dns_suffixes" {
-  type = "list"
-  description = "Name resolution suffixes for the virtual network adapter"
-}
-
-variable "MQV9Node01_dns_servers" {
-  type = "list"
-  description = "DNS servers for the virtual network adapter"
-}
-
-variable "MQV9Node01_network_interface_label" {
-  description = "vSphere port group or network label for virtual machine's vNIC"
-}
-
-variable "MQV9Node01_ipv4_gateway" {
-  description = "IPv4 gateway for vNIC configuration"
-}
-
-variable "MQV9Node01_ipv4_address" {
-  description = "IPv4 address for vNIC configuration"
-}
-
-variable "MQV9Node01_ipv4_prefix_length" {
-  description = "IPv4 prefix length for vNIC configuration. The value must be a number between 8 and 32"
-}
-
-variable "MQV9Node01_adapter_type" {
-  description = "Network adapter type for vNIC Configuration"
-  default = "vmxnet3"
-}
-
-variable "MQV9Node01_root_disk_datastore" {
-  description = "Data store or storage cluster name for target virtual machine's disks"
-}
-
-variable "MQV9Node01_root_disk_type" {
   type = "string"
-  description = "Type of template disk volume"
-  default = "eager_zeroed"
+  description = "IBMCloud datacenter where infrastructure resources will be deployed"
+  default = "dal05"
 }
 
-variable "MQV9Node01_root_disk_controller_type" {
-  type = "string"
-  description = "Type of template disk controller"
-  default = "scsi"
-}
 
-variable "MQV9Node01_root_disk_keep_on_remove" {
+#Parameter : MQV9Node01_private_network_only
+variable "MQV9Node01_private_network_only" {
   type = "string"
-  description = "Delete template disk volume when the virtual machine is deleted"
+  description = "Provision the virtual machine with only private IP"
   default = "false"
 }
 
-# vsphere vm
-resource "vsphere_virtual_machine" "MQV9Node01" {
-  name = "${var.MQV9Node01-name}"
-  domain = "${var.MQV9Node01_domain}"
-  folder = "${var.MQV9Node01_folder}"
+
+#Parameter : MQV9Node01_number_of_cores
+variable "MQV9Node01_number_of_cores" {
+  type = "string"
+  description = "Number of CPU cores, which is required to be a positive Integer"
+  default = "2"
+}
+
+
+#Parameter : MQV9Node01_memory
+variable "MQV9Node01_memory" {
+  type = "string"
+  description = "Amount of Memory (MBs), which is required to be one or more times of 1024"
+  default = "2048"
+}
+
+
+#Parameter : MQV9Node01_network_speed
+variable "MQV9Node01_network_speed" {
+  type = "string"
+  description = "Bandwidth of network communication applied to the virtual machine"
+  default = "10"
+}
+
+
+#Parameter : MQV9Node01_hourly_billing
+variable "MQV9Node01_hourly_billing" {
+  type = "string"
+  description = "Billing cycle: hourly billed or monthly billed"
+  default = "true"
+}
+
+
+#Parameter : MQV9Node01_dedicated_acct_host_only
+variable "MQV9Node01_dedicated_acct_host_only" {
+  type = "string"
+  description = "Shared or dedicated host, where dedicated host usually means higher performance and cost"
+  default = "false"
+}
+
+
+#Parameter : MQV9Node01_local_disk
+variable "MQV9Node01_local_disk" {
+  type = "string"
+  description = "User local disk or SAN disk"
+  default = "false"
+}
+
+variable "MQV9Node01_root_disk_size" {
+  type = "string"
+  description = "Root Disk Size - MQV9Node01"
+  default = "25"
+}
+
+resource "ibm_compute_vm_instance" "MQV9Node01" {
+  hostname = "${var.MQV9Node01-name}"
+  os_reference_code = "${var.MQV9Node01-image}"
+  domain = "${var.runtime_domain}"
   datacenter = "${var.MQV9Node01_datacenter}"
-  vcpu = "${var.MQV9Node01_number_of_vcpu}"
+  network_speed = "${var.MQV9Node01_network_speed}"
+  hourly_billing = "${var.MQV9Node01_hourly_billing}"
+  private_network_only = "${var.MQV9Node01_private_network_only}"
+  cores = "${var.MQV9Node01_number_of_cores}"
   memory = "${var.MQV9Node01_memory}"
-  cluster = "${var.MQV9Node01_cluster}"
-  dns_suffixes = "${var.MQV9Node01_dns_suffixes}"
-  dns_servers = "${var.MQV9Node01_dns_servers}"
-
-  network_interface {
-    label = "${var.MQV9Node01_network_interface_label}"
-    ipv4_gateway = "${var.MQV9Node01_ipv4_gateway}"
-    ipv4_address = "${var.MQV9Node01_ipv4_address}"
-    ipv4_prefix_length = "${var.MQV9Node01_ipv4_prefix_length}"
-    adapter_type = "${var.MQV9Node01_adapter_type}"
-  }
-
-  disk {
-    type = "${var.MQV9Node01_root_disk_type}"
-    template = "${var.MQV9Node01-image}"
-    datastore = "${var.MQV9Node01_root_disk_datastore}"
-    keep_on_remove = "${var.MQV9Node01_root_disk_keep_on_remove}"
-    controller_type = "${var.MQV9Node01_root_disk_controller_type}"
-  }
-
-  # Specify the connection
+  disks = ["${var.MQV9Node01_root_disk_size}"]
+  dedicated_acct_host_only = "${var.MQV9Node01_dedicated_acct_host_only}"
+  local_disk = "${var.MQV9Node01_local_disk}"
+  ssh_key_ids = ["${data.ibm_compute_ssh_key.ibm_pm_public_key.id}"]
+  # Specify the ssh connection
   connection {
-    type = "ssh"
-    user = "${var.MQV9Node01-os_admin_user}"
-    password = "${var.MQV9Node01-os_password}"
+    user = "${var.MQV9Node01-os_admin_user == "" ? lookup(var.default_os_admin_user, var.MQV9Node01-image) : var.MQV9Node01-os_admin_user}"
+    private_key = "${base64decode(var.ibm_pm_private_ssh_key)}"
   }
 
   provisioner "file" {
@@ -430,47 +428,34 @@ resource "vsphere_virtual_machine" "MQV9Node01" {
 # =================================================================
 #!/bin/bash
 
-if (( $# != 3 )); then
-echo "usage: arg 1 is user, arg 2 is public key, arg3 is CAMC Public Key"
-exit -1
+if (( $# != 2 )); then
+    echo "usage: arg 1 is user, arg 2 is public key"
+    exit -1
 fi
 
-userid="$1"
-ssh_key="$2"
-camc_ssh_key="$3"
+userid=$1
+ssh_key=$2
+
+if [[ $ssh_key = 'None' ]]; then
+  echo "skipping add, 'None' specified"
+  exit 0
+fi
 
 user_home=$(eval echo "~$userid")
 user_auth_key_file=$user_home/.ssh/authorized_keys
-echo "$user_auth_key_file"
 if ! [ -f $user_auth_key_file ]; then
-echo "$user_auth_key_file does not exist on this system, creating."
-mkdir $user_home/.ssh
-chmod 700 $user_home/.ssh
-touch $user_home/.ssh/authorized_keys
-chmod 600 $user_home/.ssh/authorized_keys
+  echo "$user_auth_key_file does not exist on this system"
+  exit -1
 else
-echo "user_home : $user_home"
+  echo "user_home --> $user_home"
 fi
 
-if [[ $ssh_key = 'None' ]]; then
-echo "skipping user key add, 'None' specified"
-else
-echo "$user_auth_key_file"
-echo "$ssh_key" >> "$user_auth_key_file"
+echo $ssh_key >> $user_auth_key_file
 if [ $? -ne 0 ]; then
-echo "failed to add to $user_auth_key_file"
-exit -1
+  echo "failed to add to $user_auth_key_file"
+  exit -1
 else
-echo "updated $user_auth_key_file"
-fi
-fi
-
-echo "$camc_ssh_key" >> "$user_auth_key_file"
-if [ $? -ne 0 ]; then
-echo "failed to add to $user_auth_key_file"
-exit -1
-else
-echo "updated $user_auth_key_file"
+  echo "updated $user_auth_key_file"
 fi
 
 EOF
@@ -480,7 +465,7 @@ EOF
   provisioner "remote-exec" {
     inline = [
       "bash -c 'chmod +x MQV9Node01_add_ssh_key.sh'",
-      "bash -c './MQV9Node01_add_ssh_key.sh  \"${var.MQV9Node01-os_admin_user}\" \"${var.user_public_ssh_key}\" \"${var.ibm_pm_public_ssh_key}\">> MQV9Node01_add_ssh_key.log 2>&1'"
+      "bash -c './MQV9Node01_add_ssh_key.sh  \"${var.MQV9Node01-os_admin_user}\" \"${var.user_public_ssh_key}\">> MQV9Node01_add_ssh_key.log 2>&1'"
     ]
   }
 
@@ -491,7 +476,7 @@ EOF
 #########################################################
 
 resource "camc_bootstrap" "MQV9Node01_chef_bootstrap_comp" {
-  depends_on = ["camc_vaultitem.VaultItem","vsphere_virtual_machine.MQV9Node01"]
+  depends_on = ["camc_vaultitem.VaultItem","ibm_compute_vm_instance.MQV9Node01"]
   name = "MQV9Node01_chef_bootstrap_comp"
   camc_endpoint = "${var.ibm_pm_service}/v1/bootstrap/chef"
   access_token = "${var.ibm_pm_access_token}"
@@ -499,10 +484,10 @@ resource "camc_bootstrap" "MQV9Node01_chef_bootstrap_comp" {
   trace = true
   data = <<EOT
 {
-  "os_admin_user": "${var.MQV9Node01-os_admin_user}",
+  "os_admin_user": "${var.MQV9Node01-os_admin_user == "default"? lookup(var.default_os_admin_user, var.MQV9Node01-image) : var.MQV9Node01-os_admin_user}",
   "stack_id": "${random_id.stack_id.hex}",
   "environment_name": "_default",
-  "host_ip": "${vsphere_virtual_machine.MQV9Node01.network_interface.0.ipv4_address}",
+  "host_ip": "${var.MQV9Node01-mgmt-network-public == "false" ? ibm_compute_vm_instance.MQV9Node01.ipv4_address_private : ibm_compute_vm_instance.MQV9Node01.ipv4_address}",
   "node_name": "${var.MQV9Node01-name}",
   "node_attributes": {
     "ibm_internal": {
@@ -532,10 +517,10 @@ resource "camc_softwaredeploy" "MQV9Node01_wmq_create_qmgrs" {
   trace = true
   data = <<EOT
 {
-  "os_admin_user": "${var.MQV9Node01-os_admin_user}",
+  "os_admin_user": "${var.MQV9Node01-os_admin_user == "default"? lookup(var.default_os_admin_user, var.MQV9Node01-image) : var.MQV9Node01-os_admin_user}",
   "stack_id": "${random_id.stack_id.hex}",
   "environment_name": "_default",
-  "host_ip": "${vsphere_virtual_machine.MQV9Node01.network_interface.0.ipv4_address}",
+  "host_ip": "${var.MQV9Node01-mgmt-network-public == "false" ? ibm_compute_vm_instance.MQV9Node01.ipv4_address_private : ibm_compute_vm_instance.MQV9Node01.ipv4_address}",
   "node_name": "${var.MQV9Node01-name}",
   "runlist": "role[wmq_create_qmgrs]",
   "node_attributes": {
@@ -588,10 +573,10 @@ resource "camc_softwaredeploy" "MQV9Node01_wmq_v9_install" {
   trace = true
   data = <<EOT
 {
-  "os_admin_user": "${var.MQV9Node01-os_admin_user}",
+  "os_admin_user": "${var.MQV9Node01-os_admin_user == "default"? lookup(var.default_os_admin_user, var.MQV9Node01-image) : var.MQV9Node01-os_admin_user}",
   "stack_id": "${random_id.stack_id.hex}",
   "environment_name": "_default",
-  "host_ip": "${vsphere_virtual_machine.MQV9Node01.network_interface.0.ipv4_address}",
+  "host_ip": "${var.MQV9Node01-mgmt-network-public == "false" ? ibm_compute_vm_instance.MQV9Node01.ipv4_address_private : ibm_compute_vm_instance.MQV9Node01.ipv4_address}",
   "node_name": "${var.MQV9Node01-name}",
   "runlist": "role[wmq_v9_install]",
   "node_attributes": {
@@ -660,7 +645,7 @@ EOT
 }
 
 output "MQV9Node01_ip" {
-  value = "VM IP Address : ${vsphere_virtual_machine.MQV9Node01.network_interface.0.ipv4_address}"
+  value = "Private : ${ibm_compute_vm_instance.MQV9Node01.ipv4_address_private} & Public : ${ibm_compute_vm_instance.MQV9Node01.ipv4_address}"
 }
 
 output "MQV9Node01_name" {
